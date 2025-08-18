3c90565912ec3ba4b00f1565ece3144efa4ace2f
Ali Raghebi <aliraghebiii@gmail.com>
2025-08-18 14:12:30 +0330

    fix

--- src/Utils/helpers.php
+++ src/Utils/helpers.php
@@ -2,10 +2,6 @@
 
 use AliRaghebi\Wallet\Utils\Number;
 
-function uuid7(): string {
-    return Str::uuid7()->toString();
-}
-
 function number(string|float|int|null $number): Number {
     return Number::of($number ?? 0);
 }
